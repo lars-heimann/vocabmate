1c545305222b016d88f5c10093e40be2f646afa1
Lars <lars@larsheimann.com>
2024-06-18 18:25:16 +0200

    fixed return type for vocabularyService

--- lib/services/vocabulary_service.dart
+++ lib/services/vocabulary_service.dart
@@ -7,8 +7,8 @@ class VocabularyService {
         await http.get(Uri.parse('http://localhost:3000/known-words/$userId'));
 
     if (response.statusCode == 200) {
-      final List<dynamic> jsonResponse = jsonDecode(response.body);
-      return jsonResponse.map((word) => word.toString()).toList();
+      final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+      return List<String>.from(jsonResponse['knownWords']);
     } else {
       throw Exception('Failed to load known words');
     }
