5374add9772512e394f6445050d3d5f633785f36
Lars Heimann <lars.heimann@viadee.de>
2024-10-25 11:01:18 +0200

    updated faq section

--- lib/pages/home_page/faq_section.dart
+++ lib/pages/home_page/faq_section.dart
@@ -10,7 +10,7 @@ import 'package:url_launcher/url_launcher.dart';
 TextStyle _getAnswerTextStyle(BuildContext context) =>
     DefaultTextStyle.of(context).style.copyWith(
           fontSize: 16,
-          color: Colors.grey[700],
+          color: const Color.fromARGB(255, 0, 0, 0),
         );
 
 class FaqSection extends ConsumerWidget {
@@ -25,30 +25,30 @@ class FaqSection extends ConsumerWidget {
         children: [
           SectionTitle(title: 'FAQ'),
           SizedBox(height: 48),
-          _IsAnkiGptOpenSource(),
+          _IsVocabmateOpenSource(),
           _WhichLanguagesAreSupported(),
           _WhichModelIsUsed(),
           _AreMyDataUsedForTraining(),
           _DoesItWorkWithOtherApps(),
           _CurrentLimits(),
           _CouldHaveFalseInformation(),
-          _IsAnkiGPTAsGPTAvailable(),
+          _IsVocabmateRelatedToAnkiGPT(),
         ],
       ),
     );
   }
 }
 
-class _IsAnkiGptOpenSource extends StatelessWidget {
-  const _IsAnkiGptOpenSource();
+class _IsVocabmateOpenSource extends StatelessWidget {
+  const _IsVocabmateOpenSource();
 
   @override
   Widget build(BuildContext context) {
     return const _FaqCard(
-      question: Text('Is the source code of AnkiGPT public?'),
+      question: Text('Is the source code of Vocabmate public?'),
       answer: _MarkdownAnswer(
         text:
-            "Yes, the source code of the client for AnkiGPT is public and can be accessed by anyone interested. You can explore or even contribute to the project by visiting the GitHub repository ([nilsreichardt/ankigpt](https://github.com/nilsreichardt/ankigpt)). However, it's important to note that while the client's code is open, the backend (including the prompts) is closed source. We greatly value community input and appreciate all contributions to improve AnkiGPT.",
+            "Yes, the source code of the client for AnkiGPT is public and can be accessed by anyone interested. You can explore or even contribute to the project by visiting the GitHub repository [lars0530/vocabmate](https://github.com/lars0530/vocabmate). However, it's important to note that while the client's code is open, the backend (including the prompts) is closed source. We greatly value community input and appreciate all contributions to improve Vocabmate. It is also important to note that this project is greatly inspired by [AnkiGPT](https://ankigpt.help/) by Nils Reichardt",
       ),
     );
   }
@@ -62,7 +62,7 @@ class _WhichLanguagesAreSupported extends StatelessWidget {
     return const _FaqCard(
       question: Text('Which languages are supported?'),
       answer: Text(
-          'AnkiGPT is designed to be a globally accessible tool, and as such, it supports nearly all languages. This broad language coverage allows users from various linguistic backgrounds to utilize AnkiGPT effectively. However, the level of support might vary depending on the language due to complexities in language structures and available datasets. We are consistently working on improving our support for all languages to ensure the best user experience possible.'),
+          'Vocabmate is designed to be a globally accessible tool, and as such, it supports nearly all languages. This broad language coverage allows users from various linguistic backgrounds to utilize Vocabmate effectively. However, the level of support might vary depending on the language due to complexities in language structures and available datasets. We are consistently working on improving our support for all languages to ensure the best user experience possible.'),
     );
   }
 }
@@ -73,10 +73,10 @@ class _WhichModelIsUsed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const _FaqCard(
-      question: Text('Which model is used for AnkiGPT?'),
+      question: Text('Which model is used for Vocabmate?'),
       answer: _MarkdownAnswer(
         text:
-            "AnkiGPT primarily utilizes the GPT-4o-mini by OpenAI model to generate flashcards, offering a seamless integration of advanced AI technology for effective learning. For users who opt for the AnkiGPT Plus version, they gain the enhanced capability to generate up to XXX flashcards per month using the more advanced [GPT-4o model](https://openai.com/index/hello-gpt-4o/), ensuring even more sophisticated and nuanced content creation.",
+            "Vocabmate primarily utilizes the GPT-4o-mini by OpenAI model to generate flashcards, offering a seamless integration of advanced AI technology for effective learning. For users who opt for the Vocabmate Plus version, they gain the enhanced capability to generate up to XXX flashcards per month using the more advanced [GPT-4o model](https://openai.com/index/hello-gpt-4o/), ensuring even more sophisticated and nuanced content creation.",
       ),
     );
   }
@@ -92,7 +92,7 @@ class _AreMyDataUsedForTraining extends StatelessWidget {
           'Is the content from my submitted lecture slides used for AI training?'),
       answer: _MarkdownAnswer(
         text:
-            "No, your submitted content is not used for AI training. AnkiGPT leverages GPT models from OpenAI, which have a strong commitment to user privacy. OpenAI do not use customer-submitted data via their API to train or improve their models (Source: [API data usage policies](https://openai.com/policies/api-data-usage-policies)). Your lecture slides content is only processed to create flashcards and is not used for any other purposes, ensuring your information remains private and secure.",
+            "No, your submitted content is not used for AI training. Vocabmate leverages GPT models from OpenAI, which have a strong commitment to user privacy. OpenAI do not use customer-submitted data via their API to train or improve their models (Source: [API data usage policies](https://openai.com/policies/api-data-usage-policies)). Your lecture slides content is only processed to create flashcards and is not used for any other purposes, ensuring your information remains private and secure.",
       ),
     );
   }
@@ -104,11 +104,11 @@ class _DoesItWorkWithOtherApps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return _FaqCard(
-      question:
-          const Text('Does AnkiGPT work with other flashcard apps than Anki?'),
+      question: const Text(
+          'Does Vocabmate work with other flashcard apps than Anki?'),
       answer: MarkdownBody(
         data:
-            "As of now, AnkiGPT is designed specifically to work with the [Anki](https://apps.ankiweb.net) flashcard app and we currently have no plans to extend support to other flashcard applications. This is to ensure the highest level of integration and functionality with [Anki](https://apps.ankiweb.net), thus providing an optimal user experience. We encourage users to utilize AnkiGPT with the [Anki](https://apps.ankiweb.net) app for the best performance and reliability.",
+            "As of now, Vocabmate is designed specifically to work with the [Anki](https://apps.ankiweb.net) flashcard app and we currently have no plans to extend support to other flashcard applications. This is to ensure the highest level of integration and functionality with [Anki](https://apps.ankiweb.net), thus providing an optimal user experience. We encourage users to utilize Vocabmate with the [Anki](https://apps.ankiweb.net) app for the best performance and reliability.",
         styleSheet: MarkdownStyleSheet(
           a: _getAnswerTextStyle(context).copyWith(
             decoration: TextDecoration.underline,
@@ -130,15 +130,15 @@ class _CurrentLimits extends StatelessWidget {
       question: Text('What are the current limitations?'),
       answer: _MarkdownAnswer(
         text:
-            """While AnkiGPT is a powerful tool, it does have a few limitations to keep in mind:
+            """While Vocabmate is a powerful tool, it does have a few limitations to keep in mind:
 
-* Firstly, remember that AI, including AnkiGPT, is not infallible. There will be occasional errors in the generated flashcards, as with any AI technology. Always review your flashcards for accuracy.
+* Firstly, remember that AI, including Vocabmate, is not infallible. There will be occasional errors in the generated flashcards, as with any AI technology. Always review your flashcards for accuracy.
 
 * Secondly, GPT models, at the current stage of development, aren't particularly adept at handling mathematical equations and formulas. This might impact the quality of flashcards in these areas.
 
 * Lastly, for users who utilize the PDF input feature, please note that it currently only processes text. Any images, including diagrams and pictures in the PDF, will be ignored.
 
-We're continually working on refining and expanding AnkiGPT's capabilities to improve your learning experience. Stay tuned for future updates and enhancements.""",
+We're continually working on refining and expanding Vocabmate's capabilities to improve your learning experience. Stay tuned for future updates and enhancements.""",
       ),
     );
   }
@@ -153,36 +153,21 @@ class _CouldHaveFalseInformation extends StatelessWidget {
       question: Text(
           'Is it possible that the flashcards have incorrect information?'),
       answer: Text(
-          "Yes, it's possible that the flashcards generated by AnkiGPT may contain inaccuracies. Even with its advanced technology, AI is not perfect and can occasionally produce errors. Therefore, AnkiGPT should be used as an assistant to your study process, not a replacement for creating your own flashcards. We recommend that users always review and cross-verify the information on the flashcards to ensure accuracy. We're continually working to improve the reliability and precision of our tool, but human oversight remains an important part of the process to guarantee quality learning outcomes."),
+          "Yes, it's possible that the flashcards generated by Vocabmate may contain inaccuracies. Even with its advanced technology, AI is not perfect and can occasionally produce errors. Therefore, Vocabmate should be used as an assistant to your study process, not a replacement for creating your own flashcards. We recommend that users always review and cross-verify the information on the flashcards to ensure accuracy. We're continually working to improve the reliability and precision of our tool, but human oversight remains an important part of the process to guarantee quality learning outcomes."),
     );
   }
 }
 
-class _IsAnkiGPTAsGPTAvailable extends StatelessWidget {
-  const _IsAnkiGPTAsGPTAvailable();
+class _IsVocabmateRelatedToAnkiGPT extends StatelessWidget {
+  const _IsVocabmateRelatedToAnkiGPT();
 
   @override
   Widget build(BuildContext context) {
     return const _FaqCard(
-      question: Text('Is AnkiGPT as GPT available?'),
+      question: Text('Is Vocabmate related to AnkiGPT?'),
       answer: _MarkdownAnswer(
         text:
-            """Yes, AnkiGPT is available as a custom GPT. Custom GPTs were introduced by OpenAI in November 2023, see [Introducing GPTs](https://openai.com/blog/introducing-gpts). Note, however, that using AnkiGPT as a GPT requires a ChatGPT Plus subscription. Otherwise, AnkiGPT as a GPT is completely free and there are no usage limits.
-
-
-**Advantages of using AnkiGPT as a GPT:**
-
-* Create unlimited flashcards without buying AnkiGPT Plus
-* Create unlimited flashcards with GPT-4o
-* Create flashcards (besides the text & PDF input) from images (e.g. screenshots of lecture slides)
-
-**Disadvantages of using AnkiGPT as a GPT:**
-
-* You need a ChatGPT Plus subscription
-* ChatGPT has a limited context windows, which means that GPT can only create 10 flashcards per message (you can ask for more flashcards when GPT is done)
-
-
-Access AnkiGPT as GPT here: [https://chatg.pt/ankigpt](https://chatg.pt/ankigpt)""",
+            "Yes, Vocabmate was heavily inspired by [AnkiGPT](ankigpt.help) a project by [Nils Reichard](https://www.linkedin.com/in/nilsreichardt)",
       ),
     );
   }
