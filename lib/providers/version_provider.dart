693adfd71568a7e7bfd7f1f2984b38e202d8e285
Lars <lars@larsheimann.com>
2024-11-20 22:21:43 +0100

    updated version_provider

--- lib/providers/version_provider.dart
+++ lib/providers/version_provider.dart
@@ -1,11 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'version_provider.g.dart';
 
 @Riverpod(dependencies: [])
-String version(VersionRef ref) {
+String version(Ref ref) {
   return const String.fromEnvironment(
     'RELEASE_DATE',
-    defaultValue: 'Jan 1 2023',
+    defaultValue: 'Nov 20 2024',
   );
 }
