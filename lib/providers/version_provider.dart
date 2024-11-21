7fe21834c3ac7118f825e600f21a16022bb58e0c
Lars <lars@larsheimann.com>
2024-11-21 14:27:57 +0100

    adddd git hash to version

--- lib/providers/version_provider.dart
+++ lib/providers/version_provider.dart
@@ -6,7 +6,7 @@ part 'version_provider.g.dart';
 @Riverpod(dependencies: [])
 String version(Ref ref) {
   return const String.fromEnvironment(
-    'RELEASE_DATE',
-    defaultValue: 'Nov 20 2024',
+    'RELEASE_DATE_VERSION',
+    defaultValue: 'Nov 21 2024 ABC',
   );
 }
