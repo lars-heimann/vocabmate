e9f0e2675c1b0c261edd358af5d2f9397c061e13
Lars Heimann <lars.heimann@ommax.com>
2025-05-19 20:10:36 +0200

    updated dev_banner to include deactivation of backend

--- lib/widgets/dev_banner.dart
+++ lib/widgets/dev_banner.dart
@@ -11,7 +11,7 @@ class DevelopmentBanner extends StatelessWidget {
       padding: const EdgeInsets.all(16.0),
       color: mediumViolet, // Use Vocabmate color
       child: const Text(
-        '⚠️ Warning: Vocabmate is still under development and some features might not yet work.',
+        '⚠️ Warning: Vocabmate is still under development and some features might not yet work.\n Due to ongoing costs in the AWS deployment of the Vocabmate backend, the service has been suspended until further notice. \n Feel free to get in touch for further information. ~Lars ',
         textAlign: TextAlign.center,
         style: TextStyle(
           color: Colors.white, // Text color
